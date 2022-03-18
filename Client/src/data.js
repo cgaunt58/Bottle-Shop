ad3079420c415ea65de912985189d8cf07d8ecaa
cgaunt58 <95644815+cgaunt58@users.noreply.github.com>
2022-03-18 04:54:28 -0400

    1st commit

---
+++ Client/src/data.js
@@ -0,0 +1,38 @@
+export const sliderItems = [
+  {
+    id: 1,
+    img: "https://images.pexels.com/photos/7083953/pexels-photo-7083953.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    title: "St. Patricks Day Sale",
+    desc: "Don't forget to prepare for the big day!",
+    bg: "fcf1ed",
+  },
+  {
+    id: 2,
+    img: "https://products1.imgix.drizly.com/ci-blantons-single-barrel-bourbon-f27c3cae7eeaa8b9.png?auto=format%2Ccompress&ch=Width%2CDPR&fm=jpg&q=20",
+    title: "WHISKEY BLOWOUT",
+    desc: "DON'T MISS OUT ON OUR NEW ARRIVALS",
+    bg: "fbf0f4",
+  },
+];
+
+export const categories = [
+  {
+    id: 1,
+    img: "https://images.pexels.com/photos/7253927/pexels-photo-7253927.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    title: "Whiskey",
+    cat:"Whiskey"
+  },
+  {
+    id: 2,
+    img: "https://images.pexels.com/photos/10140538/pexels-photo-10140538.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    title: "Vodka",
+    cat:"Vodka"
+  },
+  {
+    id: 3,
+    img: "https://images.pexels.com/photos/7601301/pexels-photo-7601301.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    title: "Tequila",
+    cat:"Tequila"
+  },
+];
+
